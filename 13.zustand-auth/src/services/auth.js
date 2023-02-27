eabfb5e1b705f7768f52d67a710d0dcb810f61a2
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-02-27 12:34:45 -0500

    Create profile request

--- 13.zustand-auth/src/services/auth.js
+++ 13.zustand-auth/src/services/auth.js
@@ -2,8 +2,9 @@ import axiosApi from '../lib/axios.js'
 
 export const csrf = async () => axiosApi.get('/sanctum/csrf-cookie')
 
+export const profileRequest = async () => await axiosApi.get('/api/user')
+
 export const loginRequest = async ({ email, password }) => {
-  await csrf()
   const payload = { email, password }
-  return axiosApi.post('/login', payload)
+  await axiosApi.post('/login', payload)
 }
