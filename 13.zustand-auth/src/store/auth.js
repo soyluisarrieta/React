4512f7be881abbd993b86376a6b76645082b6a0a
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-03-07 12:34:45 -0500

    Create auth store usign zustand

---
+++ 13.zustand-auth/src/store/auth.js
@@ -0,0 +1,12 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+export const useAuthStore = create(
+  persist(
+    (set) => ({
+      user: null,
+      setUser: (user) => set((state) => ({ user }))
+    }),
+    { name: 'auth' }
+  )
+)
