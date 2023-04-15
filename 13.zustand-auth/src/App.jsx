6bc89ab5b24600e38b14bb33d7f1ba90ea1f42b8
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-04-15 12:34:45 -0500

    Install and configure tailwind 3

--- 13.zustand-auth/src/App.jsx
+++ 13.zustand-auth/src/App.jsx
@@ -1,8 +1,8 @@
 function App () {
   return (
-    <>
-      <h1>Hi StandardJS</h1>
-    </>
+    <div className='min-h-screen bg-indigo-950'>
+      <h1 className='pt-10 text-white/90 text-6xl text-center font-semibold'>Hi Tailwind</h1>
+    </div>
   )
 }
 
