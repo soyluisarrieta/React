af57924d704907ed96f7d3ca54da113bc2b9ca8d
Luis Arrieta <luisarrieta796@gmail.com>
2023-04-28 21:22:15 -0500

    Corregir y añadir ultimos ajustes

--- midu-projects/03-mouse-follower/src/App.jsx
+++ midu-projects/03-mouse-follower/src/App.jsx
@@ -4,20 +4,29 @@ const FollowMouse = () => {
   const [enabled, setEnabled] = useState(false)
   const [position, setPosition] = useState({x: 0, y: 0})
 
+  // Pointer move
   useEffect(() => {
     const handleMove = (event) => {
       const { clientX, clientY } = event
-      setPosition({ x: clientX, y: clientY})
+      setPosition({ x: clientX, y: clientY })
     }
 
     if (enabled) {
       window.addEventListener("pointermove", handleMove)
     }
+    
+    return () => window.removeEventListener('pointermove', handleMove)
+  }, [enabled]) 
+
+  // Hide cursor
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enabled)
 
     return () => {
-      window.addEventListener('pointermove', handleMove)
+      document.body.classList.remove('no-cursor')
     }
-  }, [enabled]) 
+  })
+
   
   return (
     <>
