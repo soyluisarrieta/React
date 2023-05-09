b86245ada7c61c5b4d9dbc55d13da78b43707a17
Luis Arrieta <luisarrieta796@gmail.com>
2023-05-08 19:17:53 -0500

    Agregar un debouncer

--- midu-projects/05-react-buscador-peliculas/src/App.jsx
+++ midu-projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,7 +1,8 @@
-import { useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import './App.css'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovie'
+import debounce from 'just-debounce-it'
 
 function useSearch () {
   const [search, updateSearch] = useState('')
@@ -40,13 +41,21 @@ function App () {
   const { search, updateSearch, error } = useSearch()
   const { movies, loading, getMovies } = useMovies({ search, sort })
 
+  const debouncedGetMovies = useCallback(
+    debounce(search => {
+      getMovies({ search })
+    }, 500)
+    , [getMovies])
+
   const handleSubmit = (event) => {
     event.preventDefault()
     getMovies({ search })
   }
 
   const handleChange = (event) => {
+    const newSearch = event.target.value
     updateSearch(event.target.value)
+    debouncedGetMovies(newSearch)
   }
 
   const handleSort = () => {
