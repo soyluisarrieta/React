c8821ef685a8c9f0e8b427712fb66a4a12044091
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-08-21 22:02:32 -0500

    Configure some styles NextUI

--- shopping-list/tailwind.config.js
+++ shopping-list/tailwind.config.js
@@ -1,5 +1,11 @@
 const { nextui } = require('@nextui-org/react')
 
+const palette = {
+  primary: '#ee5533',
+  background: '#1c153d',
+  text: { light: 'rgba(0,0,0,0.88)', dark: 'rgba(255,255,255,0.85)' }
+}
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -11,5 +17,62 @@ export default {
     extend: {}
   },
   darkMode: 'class',
-  plugins: [nextui()]
+  plugins: [nextui({
+    layout: {
+      spacingUnit: 4, // in px
+      disabledOpacity: '.5', // this value is applied as opacity-[value] when the component is disabled
+      dividerWeight: '1px', // h-divider the default height applied to the divider component
+      fontSize: {
+        tiny: '0.75rem', // text-tiny
+        small: '0.875rem', // text-small
+        medium: '1rem', // text-medium
+        large: '1.125rem' // text-large
+      },
+      lineHeight: {
+        tiny: '1rem', // text-tiny
+        small: '1.25rem', // text-small
+        medium: '1.5rem', // text-medium
+        large: '1.75rem' // text-large
+      },
+      radius: {
+        small: '8px', // rounded-small
+        medium: '12px', // rounded-medium
+        large: '14px' // rounded-large
+      },
+      borderWidth: {
+        small: '1px', // border-small
+        medium: '2px', // border-medium (default)
+        large: '3px' // border-large
+      }
+    },
+
+    themes: {
+      light: {
+        colors: {
+          background: '#FFFFFF',
+          foreground: '#11181C',
+          default: '#ff0000',
+          defaultForeground: palette.text.light,
+          primary: {
+            DEFAULT: palette.primary,
+            foreground: '#000000'
+          },
+          focus: palette.primary
+        }
+      },
+      dark: {
+        colors: {
+          background: palette.background,
+          foreground: '#ECEDEE',
+          default: '#31256a',
+          'default-foreground': palette.text.dark,
+          primary: {
+            DEFAULT: palette.primary,
+            foreground: '#000000'
+          },
+          focus: palette.primary
+        }
+      }
+    }
+  })]
 }
