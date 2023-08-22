71938a567be3c8bea71a031e89a1c7a87578fb6a
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-08-21 19:39:36 -0500

    Install and configure TailwindCSS 3

--- shopping-list/src/App.jsx
+++ shopping-list/src/App.jsx
@@ -1,8 +1,8 @@
 function App () {
   return (
-    <>
-      <h1>Hi StandardJS</h1>
-    </>
+    <h1 className='text-5xl font-bold underline text-center pt-5 text-indigo-600'>
+      Hello Tailwind 3!
+    </h1>
   )
 }
 
