9f52767467ccd145accb51fb231b954232c24b55
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-08-21 19:43:43 -0500

    Configure NextUI-2

--- shopping-list/src/App.jsx
+++ shopping-list/src/App.jsx
@@ -1,8 +1,15 @@
+import { Button } from '@nextui-org/react'
+
 function App () {
   return (
-    <h1 className='text-5xl font-bold underline text-center pt-5 text-indigo-600'>
-      Hello Tailwind 3!
-    </h1>
+    <div className='w-screen h-screen flex flex-col gap-7 justify-center  items-center'>
+      <h1 className='text-5xl font-bold underline text-indigo-600'>
+        Hello Tailwind 3!
+      </h1>
+      <Button color='primary' className='w-fit' size='lg'>
+        Button
+      </Button>
+    </div>
   )
 }
 
