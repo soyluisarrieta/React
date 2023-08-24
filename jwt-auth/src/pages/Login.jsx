b98eae82614c5a7b4a5846a5c1ba63646308dcc8
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-08-24 10:55:55 -0500

    Create login and register pages

---
+++ jwt-auth/src/pages/Login.jsx
@@ -0,0 +1,7 @@
+function Login () {
+  return (
+    <h1>I'm Login page</h1>
+  )
+}
+
+export default Login
---
+++ jwt-auth/src/pages/Register.jsx
@@ -0,0 +1,7 @@
+function Register () {
+  return (
+    <h1>I'm Register page</h1>
+  )
+}
+
+export default Register
