40de843817c70120a4a72cfa18f7d96a4ead6696
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-08-24 10:53:48 -0500

    Install and configure NextUI and TailwindCSS

--- jwt-auth/src/App.jsx
+++ jwt-auth/src/App.jsx
@@ -1,9 +1,27 @@
-function App () {
+import React from 'react'
+import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button } from '@nextui-org/react'
+
+export default function App () {
   return (
-    <>
-      <h1>Hi world</h1>
-    </>
+    <main className='w-full p-5'>
+      <h1 className='mb-3 text-4xl text-indigo-600 font-bold'>Hello Tailwind</h1>
+      <Dropdown>
+        <DropdownTrigger>
+          <Button
+            variant='bordered'
+          >
+            Open Menu
+          </Button>
+        </DropdownTrigger>
+        <DropdownMenu aria-label='Static Actions'>
+          <DropdownItem key='new'>New file</DropdownItem>
+          <DropdownItem key='copy'>Copy link</DropdownItem>
+          <DropdownItem key='edit'>Edit file</DropdownItem>
+          <DropdownItem key='delete' className='text-danger' color='danger'>
+            Delete file
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    </main>
   )
 }
-
-export default App
