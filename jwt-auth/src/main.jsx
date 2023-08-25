2b9c8c56eb0583c2cf3bc27eb1fdd743c2d3c123
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-08-25 17:40:45 -0500

    Create auth context with a hook

--- jwt-auth/src/main.jsx
+++ jwt-auth/src/main.jsx
@@ -4,11 +4,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { NextUIProvider } from '@nextui-org/react'
 import App from './App'
+import AuthProvider from './contexts/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NextUIProvider>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </NextUIProvider>
   </React.StrictMode>
 )
