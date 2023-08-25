00e28e610911ce0956af75effa73d9984d338cee
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-08-24 22:05:41 -0500

    Add components with validations to login form

--- jwt-auth/src/components/Auth/LoginForm.jsx
+++ jwt-auth/src/components/Auth/LoginForm.jsx
@@ -1,38 +1,57 @@
 import { useState } from 'react'
-import { Button, Input, Link } from '@nextui-org/react'
+import { Button, Link } from '@nextui-org/react'
 import { EyeSlashFilledIcon } from '../../assets/icons/EyeSlashFilledIcon'
 import { EyeFilledIcon } from '../../assets/icons/EyeFilledIcon'
+import { Formik } from 'formik'
+import { loginValidationSchema } from './authValidationSchemas'
+import InputFormik from './InputFormik'
+
+const initialValues = {
+  email: '',
+  password: ''
+}
 
 function LoginForm () {
-  const [isVisible, setIsVisible] = useState(false)
-  const toggleVisibility = () => setIsVisible(!isVisible)
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false)
+
+  const onSubmit = (values) => {
+    console.log('Formulario enviado:', values)
+  }
 
   return (
-    <form className='w-[500px] flex flex-col gap-4'>
-      <Input
-        isClearable
-        type='email'
-        label='Email'
-        isRequired
-      />
-      <Input
-        label='Password'
-        type={isVisible ? 'text' : 'password'}
-        isRequired
-        endContent={
-          <button className='focus:outline-none' type='button' onClick={toggleVisibility}>
-            {!isVisible
-              ? (<EyeSlashFilledIcon className='text-2xl text-default-400 pointer-events-none' />)
-              : (<EyeFilledIcon className='text-2xl text-default-400 pointer-events-none' />)}
-          </button>
-      }
-      />
+    <Formik
+      initialValues={initialValues}
+      validationSchema={loginValidationSchema}
+      onSubmit={onSubmit}
+    >
+      <div className='w-[500px] flex flex-col gap-4'>
+        <InputFormik
+          isClearable
+          type='text'
+          label='Email'
+          name='email'
+          isRequired
+        />
+        <InputFormik
+          type={isPasswordVisible ? 'text' : 'password'}
+          label='Password'
+          name='password'
+          isRequired
+          endContent={
+            <button className='focus:outline-none' type='button' onClick={() => setIsPasswordVisible(!isPasswordVisible)}>
+              {!isPasswordVisible
+                ? (<EyeSlashFilledIcon className='text-2xl text-default-400 pointer-events-none' />)
+                : (<EyeFilledIcon className='text-2xl text-default-400 pointer-events-none' />)}
+            </button>
+          }
+        />
 
-      <div className='text-center'>
-        <Button className='mb-3' type='submit' fullWidth variant='solid' color='primary'>Login</Button>
-        <p className='font-light'>Are you not member? <Link className='font-semibold underline' href='#'>Sign up</Link></p>
+        <div className='text-center mt-3'>
+          <Button className='mb-3' type='submit' fullWidth color='primary'>Login</Button>
+          <p className='font-light'>Are you not member? <Link className='font-semibold underline' href='#'>Sign up</Link></p>
+        </div>
       </div>
-    </form>
+    </Formik>
   )
 }
 
