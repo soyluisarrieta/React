aebe437250fb1c45a19cdc5ffc0e0934d850e12f
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-08-24 21:45:15 -0500

    Create constants on auth validation schemas

--- jwt-auth/src/components/Auth/authValidationSchemas.js
+++ jwt-auth/src/components/Auth/authValidationSchemas.js
@@ -1,32 +1,69 @@
 import * as yup from 'yup'
 
+const MESSAGES = {
+  name: {
+    min: 'Name must be at least 3 characters',
+    required: 'Name is required'
+  },
+  lastname: {
+    min: 'Lastname must be at least 3 characters',
+    required: 'lastname is required'
+  },
+  email: {
+    required: 'Email is required',
+    format: 'Invalid email format',
+    regex: 'Invalid email address'
+  },
+  password: {
+    min: 'Password must be at least 6 characters',
+    required: 'Password is required',
+    regex: 'Must contain at least one letter and one number'
+  },
+  passwordConfirmation: {
+    required: 'Password confirmation is required',
+    notMatch: 'Passwords not match'
+  }
+}
+
+const REGEX = {
+  email: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+  password: /^(?=.*[A-Za-z])(?=.*\d)/
+}
+
+export const loginValidationSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email(MESSAGES.email.format)
+    .required(MESSAGES.email.required)
+    .matches(REGEX.email, MESSAGES.email.regex),
+  password: yup
+    .string()
+    .min(6, MESSAGES.password.min)
+    .required(MESSAGES.password.required)
+    .matches(REGEX.password, MESSAGES.password.regex)
+})
+
 export const registerValidationSchema = yup.object().shape({
   name: yup
     .string()
-    .min(3, 'Name must be at least 3 characters')
-    .required('Name is required'),
+    .min(3, MESSAGES.name.min)
+    .required(MESSAGES.name.required),
   lastname: yup
     .string()
-    .min(3, 'Lastname must be at least 3 characters')
-    .required('Lastname is required'),
+    .min(3, MESSAGES.lastname.min)
+    .required(MESSAGES.lastname.required),
   email: yup
     .string()
-    .email('Invalid email format')
-    .required('Email is required')
-    .matches(
-      /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-      'Invalid email address'
-    ),
+    .email(MESSAGES.email.format)
+    .required(MESSAGES.email.required)
+    .matches(REGEX.email, MESSAGES.email.regex),
   password: yup
     .string()
-    .min(6, 'Password must be at least 6 characters')
-    .required('Password is required')
-    .matches(
-      /^(?=.*[A-Za-z])(?=.*\d)/,
-      'Must contain at least one letter and one number'
-    ),
+    .min(6, MESSAGES.password.min)
+    .required(MESSAGES.password.required)
+    .matches(REGEX.password, MESSAGES.password.regex),
   passwordConfirmation: yup
     .string()
-    .required('Password confirmation is required')
-    .oneOf([yup.ref('password'), null], 'Passwords not match')
+    .required(MESSAGES.passwordConfirmation.required)
+    .oneOf([yup.ref('password'), null], MESSAGES.passwordConfirmation.notMatch)
 })
