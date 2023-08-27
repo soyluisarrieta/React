07b0824166f5f79b9fd482cdb3a71acfa811f650
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-08-27 10:47:42 -0500

    Add user page on routes

--- jwt-auth/src/routes/PrivateRoutes.jsx
+++ jwt-auth/src/routes/PrivateRoutes.jsx
@@ -1,10 +1,12 @@
 import AdminLayout from '../layouts/AdminLayout'
 import Dashboard from '../pages/admin/Dashboard'
+import Users from '../pages/admin/Users'
 
 const LAYOUT = { path: '/', element: <AdminLayout /> }
 const ROUTES = [
   { path: '/', element: <Dashboard /> },
-  { path: '/home', element: <Dashboard /> }
+  { path: '/home', element: <Dashboard /> },
+  { path: '/users', element: <Users /> }
 ]
 
 export default { ...LAYOUT, children: ROUTES }
