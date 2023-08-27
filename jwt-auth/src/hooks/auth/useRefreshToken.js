d2019adc90e1eacb0cc022e381025e2906eea7b0
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-08-27 17:32:59 -0500

    Fix refresh token

--- jwt-auth/src/hooks/auth/useRefreshToken.js
+++ jwt-auth/src/hooks/auth/useRefreshToken.js
@@ -4,11 +4,15 @@ import useAuth from './useAuth'
 function useRefreshToken () {
   const { auth, setAuth } = useAuth()
   const refresh = async () => {
-    const response = await axiosClient.post('/auth/refresh', {
+    const response = await axiosClient.post('/auth/refresh', null, {
+      headers: {
+        Authorization: `Bearer ${auth?.accessToken}`
+      },
       withCredentials: true
     })
-    // setAuth(prev => ({ ...prev, accessToken: response.data.access_token }))
-    return response.data.access_token
+
+    setAuth(prev => ({ ...prev, accessToken: response.data.data.authorization.access_token }))
+    return response.data.data.authorization.access_token
   }
   return refresh
 }
