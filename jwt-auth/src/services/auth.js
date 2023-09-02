a85d8f39f34b1db63d7a8989d3a1feb0faaecf0a
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-09-02 15:36:29 -0500

    Create basic login services

--- jwt-auth/src/services/auth.js
+++ jwt-auth/src/services/auth.js
@@ -1,9 +1,26 @@
 import axiosClient from '../axios.client'
 
-export const logout = async (token) => {
-  const response = await axiosClient.post('/auth/logout', { access_token: token }, {
-    withCredentials: true
-  })
-  console.log({ logout: response })
-  return true
+export const csrfService = async () => await axiosClient.get('/sanctum/csrf-cookie')
+
+export const profileService = async () => {
+  // TODO: Desestructurar los datos del usuario para definir la respuesta
+  return await axiosClient.get('/api/user')
 }
+
+export const loginService = async ({ email, password }) => {
+  const payload = { email, password }
+  return await axiosClient.post('/login', payload)
+}
+
+export const registerService = async ({ name, lastname, email, password, passwordConfirmation }) => {
+  const payload = {
+    name,
+    lastname,
+    email,
+    password,
+    password_confirmation: passwordConfirmation
+  }
+  return await axiosClient.post('/register', payload)
+}
+
+export const logoutService = async () => await axiosClient.post('/logout')
