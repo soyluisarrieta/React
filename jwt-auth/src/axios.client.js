36b2d5dfd1ef55c2e7691f1ca23d5741bfd2580e
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-09-02 15:35:45 -0500

    Add option withCredentials on axiosClient

--- jwt-auth/src/axios.client.js
+++ jwt-auth/src/axios.client.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 
-const BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/api`
+const BASE_URL = `${import.meta.env.VITE_API_BASE_URL}`
 
 const axiosClient = axios.create({
-  baseURL: BASE_URL
+  baseURL: BASE_URL,
+  withCredentials: true
 })
 
 export const axiosPrivate = axios.create({
