8c985ca21b8ddd915bd0aaac13807e1998c0ec8d
Luis Arrieta <luisarrieta796@gmail.com>
2023-09-12 11:45:35 -0500

    Fetch to API randomuser.me

--- 14.table-width-filters/src/App.jsx
+++ 14.table-width-filters/src/App.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from 'react'
+
 function App () {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api?results=100')
+      .then(async res => await res.json())
+      .then(res => {
+        setUsers(res.results)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }, [])
+
   return (
     <>
-      Hi Standard
+      <h1>Usuarios</h1>
+      {JSON.stringify(users)}
     </>
   )
 }
