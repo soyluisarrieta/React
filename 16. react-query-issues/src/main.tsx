5a04c51a80eb9eddedb15ffefa0deb1df4027fa0
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-09-19 18:36:07 -0500

    Install and configure react-query and react-query-devtools

--- 16. react-query-issues/src/main.tsx
+++ 16. react-query-issues/src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 
@@ -7,8 +9,13 @@ import { router } from './router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles.css';
 
+const queryClient = new QueryClient
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={ router } />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <RouterProvider router={ router } />
+    </QueryClientProvider>
   </React.StrictMode>
 )
