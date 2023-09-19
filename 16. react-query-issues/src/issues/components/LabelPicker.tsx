550379a7cada5ff4812e786f8e5187bc1ce0616c
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-09-19 18:47:45 -0500

    Get github labels from facebook/react usign api github

--- 16. react-query-issues/src/issues/components/LabelPicker.tsx
+++ 16. react-query-issues/src/issues/components/LabelPicker.tsx
@@ -1,5 +1,20 @@
+import { useQuery } from "@tanstack/react-query"
+
+const getLabels = async () => {
+  const res = await fetch('https://api.github.com/repos/facebook/react/labels');
+  const data = await res.json()
+  
+  console.log(data);
+  return data
+}
 
 export const LabelPicker = () => {
+
+  const labelsQuery = useQuery(
+    ['labels'],
+    getLabels
+  )
+
   return (
     <div>
         <span 
