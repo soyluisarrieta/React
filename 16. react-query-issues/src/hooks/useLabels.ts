2942c900bcef9c8f708ba9527831faf47b76b8c6
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-09-20 08:19:50 -0500

    Use some options of react-query

--- 16. react-query-issues/src/hooks/useLabels.ts
+++ 16. react-query-issues/src/hooks/useLabels.ts
@@ -12,7 +12,26 @@ function useLabels() {
     ['labels'],
     getLabels,
     {
-      staleTime: 1000 * 60 * 60, // <--- 1h
+      staleTime: 1000 * 60 * 60, // <--- 1h como mínimo para hacer refetch cuando pierde el foco
+      // initialData: [], // <--- Datos iniciales hasta que el staleTime realice un refetch
+      placeholderData: [  // <--- Datos iniciales hasta que reciba nuevos datos de la petición
+        {
+          id: 717031390,
+          node_id: "MDU6TGFiZWw3MTcwMzEzOTA=",
+          url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue",
+          name: "good first issue",
+          color: "6ce26a",
+          default: true,
+        },
+        {
+          id: 725156255,
+          node_id: "MDU6TGFiZWw3MjUxNTYyNTU=",
+          url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue%20(taken)",
+          name: "good first issue (taken)",
+          color: "b60205",
+          default: false,
+        }
+      ]
     }
   )
 
