7170b64e76804d66a570819a9ed551eda3aca96e
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-09-21 18:22:53 -0500

    Add .env and .env.example with the github api_key

--- 16. react-query-issues/src/api/githubApi.ts
+++ 16. react-query-issues/src/api/githubApi.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 export const gitHubApi = axios.create({
   baseURL: 'https://api.github.com/repos/facebook/react',
   headers: {
-    Authorization: 'Bearer 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'
+    Authorization: 'Bearer '+import.meta.env.VITE_GITHUB_API_KEY
   }
 })
