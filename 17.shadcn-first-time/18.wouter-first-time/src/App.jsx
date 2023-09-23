8e7f57bdbe8fd516d8046fdbeadd386c6b334e14
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-09-23 18:46:38 -0500

    Create simple navigation using links and dynamic routes

--- 17.shadcn-first-time/18.wouter-first-time/src/App.jsx
+++ 17.shadcn-first-time/18.wouter-first-time/src/App.jsx
@@ -1,8 +1,33 @@
-function App () {
+import { Link, Route } from 'wouter'
+
+function InboxPage () {
   return (
     <>
-      <h1>Hi!</h1>
+      <h1>Im imbox</h1>
+      <Link href='..'>Back</Link>
     </>
+  )
+}
+
+function App () {
+  return (
+    <div>
+      <header>
+        <Link href='/users/1'>
+          <a className='link'>Profile</a>
+        </Link>
+        <Link href='/about'>
+          <a className='link'>About</a>
+        </Link>
+        <Link href='/inbox'>
+          <a className='link'>Inbox</a>
+        </Link>
+      </header>
+
+      <Route path='/about'>About Us</Route>
+      <Route path='/users/:name'>{(params) => <div>Hello, {params.name}!</div>}</Route>
+      <Route path='/inbox' component={InboxPage} />
+    </div>
   )
 }
 
