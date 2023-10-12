bf5107ac67444aa4bad6dfd7ad9152dc3793edee
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-10-12 18:18:37 -0500

    init new project 'scraping-yt'

--- 12.csrf-auth/src/services/auth.js
+++ 12.csrf-auth/src/services/auth.js
@@ -1,26 +1,19 @@
 import axiosClient from '../axios.client'
 
-export const csrfService = async () => await axiosClient.get('/sanctum/csrf-cookie')
+export const csrfService = () => axiosClient.get('/sanctum/csrf-cookie')
 
-export const profileService = async () => {
+export const profileService = () => {
   // TODO: Desestructurar los datos del usuario para definir la respuesta
-  return await axiosClient.get('/api/user')
+  return axiosClient.get('/api/user')
 }
 
-export const loginService = async ({ email, password }) => {
+export const loginService = ({ email, password }) => {
   const payload = { email, password }
-  return await axiosClient.post('/login', payload)
+  return axiosClient.post('/login', payload)
 }
 
-export const registerService = async ({ name, lastname, email, password, passwordConfirmation }) => {
-  const payload = {
-    name,
-    lastname,
-    email,
-    password,
-    password_confirmation: passwordConfirmation
-  }
-  return await axiosClient.post('/register', payload)
+export const registerService = (userData) => {
+  return axiosClient.post('/register', payload)
 }
 
 export const logoutService = async () => await axiosClient.post('/logout')
--- 19.tremor-first-time/src/App.jsx
+++ 19.tremor-first-time/src/App.jsx
@@ -1,10 +1,52 @@
+import { Card, Title, AreaChart } from "@tremor/react";
 
-function App() {
-  return (
-    <>
+const chartdata = [
+  {
+    date: "Jan 22",
+    SemiAnalysis: 2890,
+    "The Pragmatic Engineer": 2338,
+  },
+  {
+    date: "Feb 22",
+    SemiAnalysis: 2756,
+    "The Pragmatic Engineer": 2103,
+  },
+  {
+    date: "Mar 22",
+    SemiAnalysis: 3322,
+    "The Pragmatic Engineer": 2194,
+  },
+  {
+    date: "Apr 22",
+    SemiAnalysis: 3470,
+    "The Pragmatic Engineer": 2108,
+  },
+  {
+    date: "May 22",
+    SemiAnalysis: 3475,
+    "The Pragmatic Engineer": 1812,
+  },
+  {
+    date: "Jun 22",
+    SemiAnalysis: 3129,
+    "The Pragmatic Engineer": 1726,
+  },
+];
 
-    </>
-  )
-}
+const dataFormatter = (number) => {
+  return "$ " + Intl.NumberFormat("us").format(number).toString();
+};
 
-export default App
+export const App = () => (
+  <Card>
+    <Title>Newsletter revenue over time (USD)</Title>
+    <AreaChart
+      className="h-72 mt-4"
+      data={chartdata}
+      index="date"
+      categories={["SemiAnalysis", "The Pragmatic Engineer"]}
+      colors={["indigo", "cyan"]}
+      valueFormatter={dataFormatter}
+    />
+  </Card>
+);
