5ce08cafc59b33ca043b9cca28f4dd1f2584f302
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-12-07 10:47:06 -0500

    Add constants

---
+++ 20.dashboard/src/constants.js
@@ -0,0 +1,10 @@
+export const APP_NAME = 'App with dashboard'
+export const APP_VERSION = '1.0.0'
+
+export const COPYRIGHT_AUTHOR = 'Paylus Estudio'
+export const COPYRIGHT_YEAR = 2023
+
+export const DEFAULT_DARKMODE = true
+
+export const API_ENDPOINT = 'http://localhost:8000'
+export const API_AVATAR_PATH = '/storage/avatars/'
