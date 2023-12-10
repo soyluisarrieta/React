c08aafba739ed5f17e6acf0da2bfa3129e3061f9
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-12-09 19:32:04 -0500

    Add paylus-estudio component

---
+++ 20.dashboard/src/assets/icons/PaylusEstudio/index.js
@@ -0,0 +1,9 @@
+import HorizontalLogo from './HorizontalLogo'
+import IconLogo from './IconLogo'
+import VerticalLogo from './VerticalLogo'
+
+export default {
+  Horizontal: HorizontalLogo,
+  Icon: IconLogo,
+  Vertical: VerticalLogo
+}
