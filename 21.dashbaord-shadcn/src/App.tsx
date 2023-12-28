06911da14a526cd403648cd9239037106d7981f5
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2023-12-27 21:13:39 -0500

    Install and configure ShadCN

--- 21.dashbaord-shadcn/src/App.tsx
+++ 21.dashbaord-shadcn/src/App.tsx
@@ -1,7 +1,12 @@
-
+import { Button } from "@/components/ui/button"
 function App() {
   return (
-    <div className="text-3xl">App</div>
+    <div className="text-3xl">
+      App 
+      <br /> 
+      <br /> 
+      <Button>Button</Button>
+      </div>
   )
 }
 
