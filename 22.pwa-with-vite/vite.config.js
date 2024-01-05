0808a8d79e012e534e0e84c46141bc6d1d0baeaf
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-01-05 15:01:22 -0500

    Use manifiest.js on vite config

--- 22.pwa-with-vite/vite.config.js
+++ 22.pwa-with-vite/vite.config.js
@@ -1,33 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa'
+import { manifestForPlugIn } from './manifest'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      manifest: {
-        name: "xd",
-        short_name: "xd2",
-        icons: [
-          {
-            src: "/android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: 'any maskable'
-          },
-          {
-            src: "/android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: 'any maskable'
-          }
-        ],
-        theme_color: "#ee5533",
-        background_color: "#191919",
-        display: "standalone"
-      }
-    })
+    VitePWA(manifestForPlugIn)
   ],
 })
