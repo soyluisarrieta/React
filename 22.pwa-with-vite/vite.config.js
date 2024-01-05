85e3f7873250daeaec9dd245ef3fc155448262d0
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-01-05 12:21:27 -0500

    Install and configure vite-plugin-pwa

--- 22.pwa-with-vite/vite.config.js
+++ 22.pwa-with-vite/vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "xd",
+        short_name: "xd2",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: 'any maskable'
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: 'any maskable'
+          }
+        ],
+        theme_color: "#ee5533",
+        background_color: "#191919",
+        display: "standalone"
+      }
+    })
+  ],
 })
