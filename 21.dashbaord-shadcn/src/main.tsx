ec93635d90141fd2b10763b4f7a72218c3794379
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-01-08 20:53:05 -0500

    Fix assertion on main.tsx

--- 21.dashbaord-shadcn/src/main.tsx
+++ 21.dashbaord-shadcn/src/main.tsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
