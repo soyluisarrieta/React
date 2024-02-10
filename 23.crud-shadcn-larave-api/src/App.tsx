6af826312add461c458046f781d495358580d034
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-02-10 13:44:15 -0500

    Add theme provider, mode toggle component and header section on App

--- 23.crud-shadcn-larave-api/src/App.tsx
+++ 23.crud-shadcn-larave-api/src/App.tsx
@@ -1,8 +1,19 @@
+import { ModeToggle } from '@/components/mode-toggle'
 import Users from '@/users'
 
 function App (): JSX.Element {
   return (
-    <Users />
+    <>
+      <header className='border-b'>
+        <div className='container flex items-center justify-between py-3 px-5'>
+          <span className='text-xl'><strong>CRUD</strong> con Shadcn y Laravel API</span>
+          <ModeToggle />
+        </div>
+      </header>
+      <main>
+        <Users />
+      </main>
+    </>
   )
 }
 
--- 23.crud-shadcn-larave-api/src/main.tsx
+++ 23.crud-shadcn-larave-api/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from '@/components/theme-provider.tsx'
 import App from './App.tsx'
 import './global.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
