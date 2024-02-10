d7e4bb286d5cb7c95082a8f6900826a9de91eb55
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-02-10 15:02:11 -0500

    Add dropdown menu options on user datatable column

--- 23.crud-shadcn-larave-api/.eslintrc.cjs
+++ 23.crud-shadcn-larave-api/.eslintrc.cjs
@@ -19,5 +19,6 @@ module.exports = {
     ],
     "rules": {
       "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-misused-promises": "off"
     }
 }
--- 23.crud-shadcn-larave-api/src/users/Columns.tsx
+++ 23.crud-shadcn-larave-api/src/users/Columns.tsx
@@ -1,5 +1,8 @@
+import { Button } from '@/components/ui/button'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 import { type Users } from '@/mocks/users'
 import { type ColumnDef } from '@tanstack/react-table'
+import { MoreHorizontalIcon } from 'lucide-react'
 
 export const columns: Array<ColumnDef<Users>> = [
   {
@@ -21,5 +24,33 @@ export const columns: Array<ColumnDef<Users>> = [
   {
     accessorKey: 'lastUpdateAt',
     header: 'Última actualización'
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      const users = row.original
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Menú de opciones</span>
+              <MoreHorizontalIcon className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem
+              onClick={async () => { await navigator.clipboard.writeText(users.id) }}
+            >
+              Copiar ID del usuario
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Ver detalles</DropdownMenuItem>
+            <DropdownMenuItem>Editar</DropdownMenuItem>
+            <DropdownMenuItem>Eliminar</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    }
   }
 ]
