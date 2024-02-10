6c62a59550a1184006fd5e2ff87b069a296eeb8b
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-02-10 13:02:54 -0500

    Add user mock

---
+++ 23.crud-shadcn-larave-api/src/mocks/users.ts
@@ -0,0 +1,912 @@
+interface Users {
+  id: string
+  name: string
+  lastname: string
+  status: boolean
+  email: string
+  emailVerifiedAt: null | number
+  lastUpdateAt: null | number
+}
+
+export const users: Users[] = [
+  {
+    id: '1d489e42',
+    name: 'Samuel',
+    lastname: 'Thompson',
+    status: true,
+    email: 'samuelthompson@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: null
+  },
+  {
+    id: '2d569g24',
+    name: 'Emily',
+    lastname: 'Johnson',
+    status: false,
+    email: 'emilyjohnson@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '3d689f34',
+    name: 'Robert',
+    lastname: 'Williams',
+    status: true,
+    email: 'robertwilliams@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: 1707587430
+  },
+  {
+    id: '4d789e44',
+    name: 'Angela',
+    lastname: 'Davis',
+    status: false,
+    email: 'angeladavis@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: null
+  },
+  {
+    id: '5d889f54',
+    name: 'George',
+    lastname: 'Miller',
+    status: true,
+    email: 'georgemiller@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '6d989g64',
+    name: 'Laura',
+    lastname: 'Wilson',
+    status: false,
+    email: 'laurawilson@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: 1707587430
+  },
+  {
+    id: '7da99h74',
+    name: 'Steven',
+    lastname: 'Moore',
+    status: true,
+    email: 'stevenmoore@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '8db99i84',
+    name: 'Jessica',
+    lastname: 'Taylor',
+    status: false,
+    email: 'jessicataylor@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: null
+  },
+  {
+    id: '9dc99j94',
+    name: 'Daniel',
+    lastname: 'Anderson',
+    status: true,
+    email: 'danielanderson@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587430
+  },
+  {
+    id: '10dd99k04',
+    name: 'Megan',
+    lastname: 'Thomas',
+    status: false,
+    email: 'meganthomas@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '11dd99l14',
+    name: 'Richard',
+    lastname: 'Jackson',
+    status: true,
+    email: 'richardjackson@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '12dd99m24',
+    name: 'Patricia',
+    lastname: 'White',
+    status: false,
+    email: 'patriciawhite@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: null
+  },
+  {
+    id: '13dd99n34',
+    name: 'Linda',
+    lastname: 'Harris',
+    status: true,
+    email: 'lindaharris@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587430
+  },
+  {
+    id: '14dd99o44',
+    name: 'Michael',
+    lastname: 'Martin',
+    status: false,
+    email: 'michaelmartin@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '15dd99p54',
+    name: 'Susan',
+    lastname: 'Thompson',
+    status: true,
+    email: 'susanthompson@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '16dd99q64',
+    name: 'Dorothy',
+    lastname: 'Garcia',
+    status: false,
+    email: 'dorothygarcia@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: null
+  },
+  {
+    id: '17dd99r74',
+    name: 'Frank',
+    lastname: 'Martinez',
+    status: true,
+    email: 'frankmartinez@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587430
+  },
+  {
+    id: '18dd99s84',
+    name: 'Nancy',
+    lastname: 'Robinson',
+    status: false,
+    email: 'nancyrobinson@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '19dd99t94',
+    name: 'Paul',
+    lastname: 'Clark',
+    status: true,
+    email: 'paulclark@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '20dd99u04',
+    name: 'Jennifer',
+    lastname: 'Rodriguez',
+    status: false,
+    email: 'jenniferrodriguez@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: null
+  },
+  {
+    id: '21dd99v14',
+    name: 'Thomas',
+    lastname: 'Lewis',
+    status: true,
+    email: 'thomaslewis@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587430
+  },
+  {
+    id: '22dd99w24',
+    name: 'Karen',
+    lastname: 'Lee',
+    status: false,
+    email: 'karenlee@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '23dd99x34',
+    name: 'Brian',
+    lastname: 'Walker',
+    status: true,
+    email: 'brianwalker@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '24dd99y44',
+    name: 'Helen',
+    lastname: 'Hall',
+    status: false,
+    email: 'helenhall@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: null
+  },
+  {
+    id: '25dd99z54',
+    name: 'David',
+    lastname: 'Allen',
+    status: true,
+    email: 'davidallen@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587430
+  },
+  {
+    id: '26dd99a64',
+    name: 'Elizabeth',
+    lastname: 'Young',
+    status: false,
+    email: 'elizabethyoung@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '27dd99b74',
+    name: 'Matthew',
+    lastname: 'Hernandez',
+    status: true,
+    email: 'matthewhernandez@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '28dd99c84',
+    name: 'Barbara',
+    lastname: 'King',
+    status: false,
+    email: 'barbaraking@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: null
+  },
+  {
+    id: '29dd99d94',
+    name: 'James',
+    lastname: 'Wright',
+    status: true,
+    email: 'jameswright@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587430
+  },
+  {
+    id: '30dd99e04',
+    name: 'Mary',
+    lastname: 'Lopez',
+    status: false,
+    email: 'marylopez@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '31dd99f14',
+    name: 'Joseph',
+    lastname: 'Hill',
+    status: true,
+    email: 'josephhill@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '32dd99g24',
+    name: 'Sandra',
+    lastname: 'Scott',
+    status: false,
+    email: 'sandrascott@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: null
+  },
+  {
+    id: '33dd99h34',
+    name: 'Charles',
+    lastname: 'Green',
+    status: true,
+    email: 'charlesgreen@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587430
+  },
+  {
+    id: '34dd99i44',
+    name: 'Patricia',
+    lastname: 'Adams',
+    status: false,
+    email: 'patriciaadams@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '35dd99j54',
+    name: 'Linda',
+    lastname: 'Baker',
+    status: true,
+    email: 'lindabaker@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '36dd99k64',
+    name: 'Robert',
+    lastname: 'Nelson',
+    status: false,
+    email: 'robertnelson@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: null
+  },
+  {
+    id: '37dd99l74',
+    name: 'Michael',
+    lastname: 'Carter',
+    status: true,
+    email: 'michaelcarter@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587430
+  },
+  {
+    id: '38dd99m84',
+    name: 'Elizabeth',
+    lastname: 'Mitchell',
+    status: false,
+    email: 'elizabethmitchell@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '39dd99n94',
+    name: 'David',
+    lastname: 'Perez',
+    status: true,
+    email: 'davidperez@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '40dd99o04',
+    name: 'Jennifer',
+    lastname: 'Roberts',
+    status: false,
+    email: 'jenniferroberts@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: null
+  },
+  {
+    id: '41dd99p14',
+    name: 'Margaret',
+    lastname: 'Turner',
+    status: true,
+    email: 'margaretturner@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587430
+  },
+  {
+    id: '42dd99q24',
+    name: 'Thomas',
+    lastname: 'Phillips',
+    status: false,
+    email: 'thomasphillips@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '43dd99r34',
+    name: 'Charles',
+    lastname: 'Campbell',
+    status: true,
+    email: 'charlescampbell@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '44dd99s44',
+    name: 'Dorothy',
+    lastname: 'Parker',
+    status: false,
+    email: 'dorothyparker@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: null
+  },
+  {
+    id: '45dd99t54',
+    name: 'John',
+    lastname: 'Evans',
+    status: true,
+    email: 'johnevans@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587430
+  },
+  {
+    id: '46dd99u64',
+    name: 'Helen',
+    lastname: 'Edwards',
+    status: false,
+    email: 'helenedwards@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '47dd99v74',
+    name: 'George',
+    lastname: 'Collins',
+    status: true,
+    email: 'georgecollins@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '48dd99w84',
+    name: 'Susan',
+    lastname: 'Stewart',
+    status: false,
+    email: 'susanstewart@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: null
+  },
+  {
+    id: '49dd99x94',
+    name: 'Frank',
+    lastname: 'Sanchez',
+    status: true,
+    email: 'franksanchez@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587430
+  },
+  {
+    id: '50dd99y04',
+    name: 'Joseph',
+    lastname: 'Morris',
+    status: false,
+    email: 'josephmorris@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '51dd99z14',
+    name: 'Margaret',
+    lastname: 'Rogers',
+    status: true,
+    email: 'margaretrogers@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '52dd99a24',
+    name: 'Edward',
+    lastname: 'Reed',
+    status: false,
+    email: 'edwardreed@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: null
+  },
+  {
+    id: '53dd99b34',
+    name: 'Ruth',
+    lastname: 'Cook',
+    status: true,
+    email: 'ruthcook@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587430
+  },
+  {
+    id: '54dd99c44',
+    name: 'William',
+    lastname: 'Morgan',
+    status: false,
+    email: 'williammorgan@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '55dd99d54',
+    name: 'Barbara',
+    lastname: 'Bell',
+    status: true,
+    email: 'barbarabell@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '56dd99e64',
+    name: 'Michael',
+    lastname: 'Murphy',
+    status: false,
+    email: 'michaelmurphy@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: null
+  },
+  {
+    id: '57dd99f74',
+    name: 'Elizabeth',
+    lastname: 'Bailey',
+    status: true,
+    email: 'elizabethbailey@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587430
+  },
+  {
+    id: '58dd99g84',
+    name: 'James',
+    lastname: 'Rivera',
+    status: false,
+    email: 'jamesrivera@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '59dd99h94',
+    name: 'Patricia',
+    lastname: 'Cooper',
+    status: true,
+    email: 'patriciacooper@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '60dd99i04',
+    name: 'Robert',
+    lastname: 'Richardson',
+    status: false,
+    email: 'robertrichardson@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: null
+  },
+  {
+    id: '61dd99j14',
+    name: 'Daniel',
+    lastname: 'Cox',
+    status: true,
+    email: 'danielcox@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '62dd99k24',
+    name: 'Carol',
+    lastname: 'Howard',
+    status: false,
+    email: 'carolhoward@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: null
+  },
+  {
+    id: '63dd99l34',
+    name: 'Paul',
+    lastname: 'Ward',
+    status: true,
+    email: 'paulward@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587430
+  },
+  {
+    id: '64dd99m44',
+    name: 'Mark',
+    lastname: 'Torres',
+    status: false,
+    email: 'marktorres@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '65dd99n54',
+    name: 'Laura',
+    lastname: 'Peterson',
+    status: true,
+    email: 'laurapeterson@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '66dd99o64',
+    name: 'Frank',
+    lastname: 'Gray',
+    status: false,
+    email: 'frankgray@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: null
+  },
+  {
+    id: '67dd99p74',
+    name: 'Donald',
+    lastname: 'Ramirez',
+    status: true,
+    email: 'donaldramirez@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587430
+  },
+  {
+    id: '68dd99q84',
+    name: 'Michelle',
+    lastname: 'James',
+    status: false,
+    email: 'michellejames@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '69dd99r94',
+    name: 'Anthony',
+    lastname: 'Watson',
+    status: true,
+    email: 'anthonywatson@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '70dd99s04',
+    name: 'Sarah',
+    lastname: 'Brooks',
+    status: false,
+    email: 'sarahbrooks@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: null
+  },
+  {
+    id: '71dd99t14',
+    name: 'Kenneth',
+    lastname: 'Kelly',
+    status: true,
+    email: 'kennethkelly@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587430
+  },
+  {
+    id: '72dd99u24',
+    name: 'Nancy',
+    lastname: 'Sanders',
+    status: false,
+    email: 'nancysanders@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '73dd99v34',
+    name: 'Steven',
+    lastname: 'Price',
+    status: true,
+    email: 'stevenprice@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '74dd99w44',
+    name: 'Jessica',
+    lastname: 'Bennett',
+    status: false,
+    email: 'jessicabennett@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: null
+  },
+  {
+    id: '75dd99x54',
+    name: 'Edward',
+    lastname: 'Wood',
+    status: true,
+    email: 'edwardwood@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587430
+  },
+  {
+    id: '76dd99y64',
+    name: 'Karen',
+    lastname: 'Barnes',
+    status: false,
+    email: 'karenbarnes@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '77dd99z74',
+    name: 'Brian',
+    lastname: 'Ross',
+    status: true,
+    email: 'brianross@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '78dd99a84',
+    name: 'Dorothy',
+    lastname: 'Henderson',
+    status: false,
+    email: 'dorothyhenderson@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: null
+  },
+  {
+    id: '79dd99b94',
+    name: 'Ronald',
+    lastname: 'Coleman',
+    status: true,
+    email: 'ronaldcoleman@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587430
+  },
+  {
+    id: '80dd99c04',
+    name: 'Shirley',
+    lastname: 'Jenkins',
+    status: false,
+    email: 'shirleyjenkins@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '81dd99d14',
+    name: 'Cynthia',
+    lastname: 'Perry',
+    status: true,
+    email: 'cynthiaperry@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '82dd99e24',
+    name: 'Charles',
+    lastname: 'Powell',
+    status: false,
+    email: 'charlespowell@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: null
+  },
+  {
+    id: '83dd99f34',
+    name: 'Kathy',
+    lastname: 'Long',
+    status: true,
+    email: 'kathylong@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587430
+  },
+  {
+    id: '84dd99g44',
+    name: 'Roger',
+    lastname: 'Patterson',
+    status: false,
+    email: 'rogerpatterson@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '85dd99h54',
+    name: 'Pamela',
+    lastname: 'Hughes',
+    status: true,
+    email: 'pamelahughes@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '86dd99i64',
+    name: 'Raymond',
+    lastname: 'Flores',
+    status: false,
+    email: 'raymondflores@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: null
+  },
+  {
+    id: '87dd99j74',
+    name: 'Sandra',
+    lastname: 'Washington',
+    status: true,
+    email: 'sandrawashington@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587430
+  },
+  {
+    id: '88dd99k84',
+    name: 'Donald',
+    lastname: 'Butler',
+    status: false,
+    email: 'donaldbutler@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '89dd99l94',
+    name: 'Susan',
+    lastname: 'Simmons',
+    status: true,
+    email: 'susansimmons@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '90dd99m04',
+    name: 'Paul',
+    lastname: 'Foster',
+    status: false,
+    email: 'paulfoster@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: null
+  },
+  {
+    id: '91dd99n14',
+    name: 'Betty',
+    lastname: 'Gonzales',
+    status: true,
+    email: 'bettygonzales@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587430
+  },
+  {
+    id: '92dd99o24',
+    name: 'Charles',
+    lastname: 'Bryant',
+    status: false,
+    email: 'charlesbryant@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '93dd99p34',
+    name: 'Linda',
+    lastname: 'Alexander',
+    status: true,
+    email: 'lindaalexander@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '94dd99q44',
+    name: 'Michael',
+    lastname: 'Russell',
+    status: false,
+    email: 'michaelrussell@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: null
+  },
+  {
+    id: '95dd99r54',
+    name: 'Sharon',
+    lastname: 'Griffin',
+    status: true,
+    email: 'sharongriffin@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587430
+  },
+  {
+    id: '96dd99s64',
+    name: 'Steven',
+    lastname: 'Diaz',
+    status: false,
+    email: 'stevendiaz@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '97dd99t74',
+    name: 'Jessica',
+    lastname: 'Hayes',
+    status: true,
+    email: 'jessicahayes@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587438
+  },
+  {
+    id: '98dd99u84',
+    name: 'Thomas',
+    lastname: 'Myers',
+    status: false,
+    email: 'thomasmyers@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: null
+  },
+  {
+    id: '99dd99v94',
+    name: 'Sarah',
+    lastname: 'Ford',
+    status: true,
+    email: 'sarahford@gmail.com',
+    emailVerifiedAt: null,
+    lastUpdateAt: 1707587430
+  },
+  {
+    id: '100dd99w04',
+    name: 'Paul',
+    lastname: 'Hamilton',
+    status: false,
+    email: 'paulhamilton@gmail.com',
+    emailVerifiedAt: 1707587440,
+    lastUpdateAt: 1707587438
+  }
+]
