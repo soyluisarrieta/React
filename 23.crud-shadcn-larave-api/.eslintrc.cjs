949869b4ab8f9adf3b7d851474a1a342f46a68b4
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-02-10 11:38:53 -0500

    Install and configure linter with ts-standard and clean project

--- 23.crud-shadcn-larave-api/.eslintrc.cjs
+++ 23.crud-shadcn-larave-api/.eslintrc.cjs
@@ -1,18 +1,23 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "standard-with-typescript"
     ],
-  },
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "project": "./tsconfig.json"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+      "react/react-in-jsx-scope": "off"
+    }
 }
