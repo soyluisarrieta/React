bd5304181376eb789e1ca5f871d485157f2fa3a2
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-02-10 12:10:48 -0500

    Install and configure TailwindCSS and Shadcn

--- 23.crud-shadcn-larave-api/src/App.tsx
+++ 23.crud-shadcn-larave-api/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 function App (): JSX.Element {
   return (
     <>
       <h1 className='text-6xl'>Hi</h1>
+      <Button>Shadcn</Button>
     </>
   )
 }
