0ea23b7b0c3a26b5a1e3d75b77f922b45a8e9962
Luis Arrieta <luisarrieta796@gmail.com>
2024-06-10 09:33:18 -0500

    Move map component to app.jsx

--- 26.react-leaflet/src/App.jsx
+++ 26.react-leaflet/src/App.jsx
@@ -1,7 +1,67 @@
-import { ExternalState } from "./components/leaflet/ExternalState";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
 
-export default function App() {
+// Coordenadas centrales y nivel de zoom inicial del mapa
+const center = [0.8180362, -77.5892683];
+const zoom = 15;
+
+// Comp: muestra la posición actual del mapa
+function DisplayPosition({ map }) {
+  const [position, setPosition] = useState(() => map.getCenter()); // Posición actual del mapa
+
+  // Fn: Reiniciar la vista del mapa a la posición y zoom inicial
+  const onReset = useCallback(() => {
+    map.setView(center, zoom);
+  }, [map]);
+
+  // Fn: actualizar la posición cuando el mapa se mueve
+  const onMove = useCallback(() => {
+    setPosition(map.getCenter());
+  }, [map]);
+
+  // Efecto secundario al evento de movimiento del mapa
+  useEffect(() => {
+    map.on('move', onMove);
+    return () => {
+      map.off('move', onMove);
+    };
+  }, [map, onMove]);
+
   return (
-    <ExternalState />
-  )
+    <p style={{ padding: '1rem' }}>
+      Latitud: {position.lat.toFixed(4)}, longitud: {position.lng.toFixed(4)}{' '}
+      <button onClick={onReset}>Reiniciar</button>
+    </p>
+  );
+}
+
+// Comp: maneja el estado del mapa y renderiza el contenedor del mapa
+export default function App() {
+  // Estado para almacenar la referencia del mapa
+  const [map, setMap] = useState(null);
+
+  // Memoizar el contenedor del mapa para evitar renderizaciones innecesarias
+  const displayMap = useMemo(
+    () => (
+      <MapContainer
+        center={center}
+        zoom={zoom}
+        ref={setMap}
+        style={{ height: '90vh' }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    ),
+    [], // Dependencias vacías para memoizar solo una vez
+  );
+
+  return (
+    <div>
+      {map ? <DisplayPosition map={map} /> : null}
+      {displayMap}
+    </div>
+  );
 }
