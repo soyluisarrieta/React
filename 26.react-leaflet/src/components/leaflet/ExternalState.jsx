2dd7635bc42e9a51b80f2b7624e4317581077857
Luis Arrieta <luisarrieta796@gmail.com>
2024-06-10 08:50:14 -0500

    Create external state component with leaflet map

--- 26.react-leaflet/src/components/leaflet/ExternalState.jsx
+++ 26.react-leaflet/src/components/leaflet/ExternalState.jsx
@@ -1,58 +1,67 @@
-import { useCallback, useEffect, useMemo, useState } from "react"
-import { MapContainer, TileLayer } from "react-leaflet"
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
 
-const center = [0.8216, 282.3678]
-const zoom = 15
+// Coordenadas centrales y nivel de zoom inicial del mapa
+const center = [0.8180362, -77.5892683];
+const zoom = 15;
 
+// Comp: muestra la posición actual del mapa
 function DisplayPosition({ map }) {
-  const [position, setPosition] = useState(() => map.getCenter())
+  const [position, setPosition] = useState(() => map.getCenter()); // Posición actual del mapa
 
-  const onClick = useCallback(() => {
-    map.setView(center, zoom)
-  }, [map])
+  // Fn: Reiniciar la vista del mapa a la posición y zoom inicial
+  const onReset = useCallback(() => {
+    map.setView(center, zoom);
+  }, [map]);
 
+  // Fn: actualizar la posición cuando el mapa se mueve
   const onMove = useCallback(() => {
-    setPosition(map.getCenter())
-  }, [map])
+    setPosition(map.getCenter());
+  }, [map]);
 
+  // Efecto secundario al evento de movimiento del mapa
   useEffect(() => {
-    map.on('move', onMove)
+    map.on('move', onMove);
     return () => {
-      map.off('move', onMove)
-    }
-  }, [map, onMove])
+      map.off('move', onMove);
+    };
+  }, [map, onMove]);
 
   return (
-    <p style={{padding: '1rem'}}>
+    <p style={{ padding: '1rem' }}>
       Latitud: {position.lat.toFixed(4)}, longitud: {position.lng.toFixed(4)}{' '}
-      <button onClick={onClick}>Reiniciar</button>
+      <button onClick={onReset}>Reiniciar</button>
     </p>
-  )
+  );
 }
 
+// Comp: maneja el estado del mapa y renderiza el contenedor del mapa
 export function ExternalState() {
-  const [map, setMap] = useState(null)
+  // Estado para almacenar la referencia del mapa
+  const [map, setMap] = useState(null);
 
+  // Memoizar el contenedor del mapa para evitar renderizaciones innecesarias
   const displayMap = useMemo(
     () => (
       <MapContainer
         center={center}
         zoom={zoom}
         ref={setMap}
-        style={{height: '90vh'}}>
+        style={{ height: '90vh' }}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
       </MapContainer>
     ),
-    [],
-  )
+    [], // Dependencias vacías para memoizar solo una vez
+  );
 
   return (
     <div>
       {map ? <DisplayPosition map={map} /> : null}
       {displayMap}
     </div>
-  )
+  );
 }
