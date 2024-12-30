66645ff3d184af06e6232ca821078e893f951ad7
Luis Arrieta <luisarrieta796@gmail.com>
2024-12-30 18:15:03 -0500

    Install and configure marks, action-menu-list and toolbar

--- 27.texteditor-yoopta/src/Editor.jsx
+++ 27.texteditor-yoopta/src/Editor.jsx
@@ -1,8 +1,29 @@
 import { useMemo, useState } from "react";
 import YooptaEditor, { createYooptaEditor } from "@yoopta/editor";
 import Paragraph from "@yoopta/paragraph";
+import LinkTool, { DefaultLinkToolRender } from '@yoopta/link-tool';
+import ActionMenu, { DefaultActionMenuRender } from '@yoopta/action-menu-list';
+import Toolbar, { DefaultToolbarRender } from '@yoopta/toolbar';
+import { Bold, Italic, CodeMark, Underline, Strike, Highlight } from '@yoopta/marks';
 
-const plugins = [Paragraph];
+const plugins = [Paragraph]
+
+const MARKS = [Bold, Italic, CodeMark, Underline, Strike, Highlight];
+const TOOLS = {
+  Toolbar: {
+    tool: Toolbar,
+    render: DefaultToolbarRender,
+  },
+  ActionMenu: {
+    tool: ActionMenu,
+    render: DefaultActionMenuRender,
+  },
+  LinkTool: {
+    tool: LinkTool,
+    render: DefaultLinkToolRender,
+  },
+};
+
 
 export default function Editor() {
   const editor = useMemo(() => createYooptaEditor(), [])
@@ -20,6 +41,8 @@ export default function Editor() {
       placeholder="Escribe aquí..."
       value={value}
       onChange={onChange}
+      marks={MARKS}
+      tools={TOOLS}
     />
   );
 };
