7227f6f294e04e92bee8970e1c2bc854b2155ccb
Luis Arrieta <luisarrieta796@gmail.com>
2024-12-31 07:36:13 -0500

    Tempt the events service

--- 28.calendar-schedule-x/src/Calendar.jsx
+++ 28.calendar-schedule-x/src/Calendar.jsx
@@ -20,20 +20,49 @@ function Calendar() {
     locale: 'es-ES',
     isDark: true,
     views: [createViewDay(), createViewWeek(), createViewMonthGrid(), createViewMonthAgenda()],
-    events: [
-      {
-        id: '1',
-        title: 'Event 1',
-        start: '2025-01-01',
-        end: '2025-01-14',
-      },
-    ],
     plugins: [eventsService, createEventModalPlugin(), createDragAndDropPlugin(), createResizePlugin()]
   })
  
   useEffect(() => {
     eventsService.getAll()
   }, [])
+
+  // Agrega un evento
+  calendar.eventsService.add({
+    title: 'Event 1',
+    description: 'This is a test event 1',
+    start: '2024-12-30',
+    end: '2025-01-01',
+    id: 1
+  })
+
+  calendar.eventsService.add({
+    title: 'Event 2',
+    description: 'This is a test event 2',
+    start: '2025-01-02',
+    end: '2025-01-03',
+    id: 2
+  })
+   
+  // Obtiene el evento por su id
+  const date = calendar.eventsService.get(1) 
+  console.log({date});
+   
+  // Obtiene todos los eventos
+  const allDates = calendar.eventsService.getAll()
+  console.log({allDates});
+   
+  // Actualiza un evento
+  calendar.eventsService.update({
+    title: 'Event updated',
+    description: 'This is a test event updated',
+    start: '2025-01-02',
+    end: '2025-01-03',
+    id: 1
+  })
+   
+  // Elimina un evento
+  calendar.eventsService.remove(2)
  
   return (
     <div id='calendar'>
