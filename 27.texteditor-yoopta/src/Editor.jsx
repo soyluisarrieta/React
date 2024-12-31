dd8c6cdd536662694e14411275a157b0a150203f
Luis Arrieta <luisarrieta796@gmail.com>
2024-12-30 19:47:28 -0500

    Add and organize others plugins

--- 27.texteditor-yoopta/src/Editor.jsx
+++ 27.texteditor-yoopta/src/Editor.jsx
@@ -1,22 +1,52 @@
 import { useMemo, useState } from "react";
 import YooptaEditor, { createYooptaEditor } from "@yoopta/editor";
-import Accordion from "@yoopta/accordion";
-import Paragraph from "@yoopta/paragraph";
-import Link from "@yoopta/link";
 import LinkTool, { DefaultLinkToolRender } from '@yoopta/link-tool';
 import ActionMenu, { DefaultActionMenuRender } from '@yoopta/action-menu-list';
 import Toolbar, { DefaultToolbarRender } from '@yoopta/toolbar';
 import { Bold, Italic, CodeMark, Underline, Strike, Highlight } from '@yoopta/marks';
+import { NumberedList, BulletedList, TodoList } from '@yoopta/lists';
+import { HeadingOne, HeadingThree, HeadingTwo } from '@yoopta/headings';
+import Accordion from "@yoopta/accordion";
+import Paragraph from "@yoopta/paragraph";
+import Link from "@yoopta/link";
+import Blockquote from "@yoopta/blockquote";
+import Callout from "@yoopta/callout";
+import Code from "@yoopta/code";
+import Divider from "@yoopta/divider";
+import Embed from "@yoopta/embed";
+import Table from "@yoopta/table";
 
 // Plugins
 const plugins = [
+  // Texto básico
   Paragraph.extend({ options: { display: { 
     title: 'Párrafo', 
     description: 'Comience a escribir texto simple.' 
-  }}}), 
-
+  }}}),
+  
+  // Encabezados
+  HeadingOne,
+  HeadingTwo, 
+  HeadingThree,
+  
+  // Listas
+  NumberedList,
+  BulletedList,
+  TodoList,
+  
+  // Elementos de formato
+  Blockquote,
+  Callout,
+  Code,
   Link,
-  Accordion 
+  
+  // Elementos estructurales
+  Accordion,
+  Divider,
+  Table,
+  
+  // Contenido embebido
+  Embed,
 ];
 
 // Marks
