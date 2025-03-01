36e719daaf543505b2122a6c4da32d4b86d0183c
Luis Arrieta <luisarrieta796@gmail.com>
2025-03-01 13:08:19 -0500

    Install and configure ShadCN

--- 29.first-time-aceternity-ui/src/App.tsx
+++ 29.first-time-aceternity-ui/src/App.tsx
@@ -1,5 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center text-4xl bg-slate-950 text-slate-100 font-bold">Hi Tailwind</div>
+    <div className="dark min-h-screen flex items-center justify-center flex-col gap-2 bg-background">
+      <h1 className="text-4xl text-slate-100 font-bold"></h1>
+      <Button>Hi Shadcn</Button>
+    </div>
   )
 }
--- 29.first-time-aceternity-ui/vite.config.ts
+++ 29.first-time-aceternity-ui/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
